c6b9e9d730796812d1a84e5755d388445c9bd01f
Chu Seng <yongchuseng@gmail.com>
2023-04-13 16:13:50 +0800

    Add z-index to navLinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <nav className='w-full flex py-6 justify-between items-center navbar'>
       <img src={logo} alt="hookbank" className='w-[124px] h-[32px]' />
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-        {navLinks.map((nav, index) => (<li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
+        {navLinks.map((nav, index) => (<li key={nav.id} className={`font-poppins font-normal cursor-pointer z-[9999] text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
           <a href={`#${nav.id}`}>{nav.title}</a>
         </li>))}
       </ul>
